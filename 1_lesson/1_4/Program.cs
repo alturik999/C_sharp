633a3d2b63d552c74a07d3bd9512b3dc6aebbd31
Артур Алтынчурин <alturik9@yandex.ru>
2022-11-22 00:51:30 +0500

    task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// 4. Напишите программу вычисления модуля числа.
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
+
