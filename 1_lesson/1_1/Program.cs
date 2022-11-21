3d5ececf24741930f24f7c59065637a57f98d17e
Артур Алтынчурин <alturik9@yandex.ru>
2022-11-21 23:56:09 +0500

    task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("number1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("number2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("да");
+}
+
+else
+ Console.WriteLine("нет");
