2b3e16c9d34e5472e8347b8304d99afca6f6f62f
Артур Алтынчурин <alturik9@yandex.ru>
2022-11-21 23:36:55 +0500

    task 1_0

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num * num);
+
