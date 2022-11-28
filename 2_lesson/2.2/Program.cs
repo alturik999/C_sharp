f498aea808e5066353263319dfe7630e711072dd
Артур Алтынчурин <alturik9@yandex.ru>
2022-11-28 21:19:38 +0500

    lesson2

---
+++ 2_lesson/2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string crat(int num,int num2)
+{
+    if (num % num2 == 0)
+        return "кратно";
+    else
+        return $"некратно, остаток = {num % num2}";
+}
+Console.WriteLine("1 chislo");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("2 chislo");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(crat(first, second));
